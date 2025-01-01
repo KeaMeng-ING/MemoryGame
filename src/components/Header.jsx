bd29235274011856f0997cfab00cff5c4b534673
KeaMeng <2023484ing@aupp.edu.kh>
2025-01-01 22:49:09 +0700

    Enhence UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,14 @@ export default function Header({ score, highScore }) {
   return (
     <header>
       <div className="header">
-        <img
-          src="https://pokemon-memory-game-dtb.pages.dev/assets/pokeLogo-7fTHLA1f.png"
-          alt="logo"
-        />
+        <div className="logo">
+          <img
+            src="https://pokemon-memory-game-dtb.pages.dev/assets/pokeLogo-7fTHLA1f.png"
+            alt="logo"
+          />
+          <p>Do not click the same card twice!</p>
+        </div>
+
         <div className="scoreboard">
           <h2>Score: {score}</h2>
           <h2>High Score: {highScore}</h2>
